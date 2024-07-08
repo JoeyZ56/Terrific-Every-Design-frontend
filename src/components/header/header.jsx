9568a2de68e62521118ff06b1aa75d7337fce607
username <Jzazzi56@gmail.com>
2024-07-08 14:22:30 -0700

    basic foundation of website

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+const header = () => {
+  return (
+    <div className="main-header-container">
+      <h1 className="main-header">Ted&apos;s Fabrication</h1>;
+      <img src="" alt="profile-image" />
+    </div>
+  );
+};
+
+export default header;
