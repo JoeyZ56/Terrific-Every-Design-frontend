806bacd89cdcff5be56731f83af50e4829ca48de
username <Jzazzi56@gmail.com>
2024-07-10 15:54:28 -0700

    ticket form part one

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/home";
+import TicketForm from "./components/ticket-form/ticketForm";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/ticketform" element={<TicketForm />} />
       </Routes>
     </Router>
   );
