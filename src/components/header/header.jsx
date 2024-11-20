ea0e4c6dd0587d6d0633a965211f67fa5471ea92
username <Jzazzi56@gmail.com>
2024-11-20 09:27:34 -0800

    removing code for logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,15 @@
 import "./header.css";
-import placeholderImage from "../../assets/placeholders/profile-image.png";
+// import placeholderImage from "../../assets/placeholders/profile-image.png";
 
 const header = () => {
   return (
     <div className="main-header-container">
       {/* <h1 className="main-header">Terrific Every Design</h1>; */}
-      <img
+      {/* <img
         src={placeholderImage}
         alt="profile-image"
         className="header-image"
-      />
+      /> */}
       <h3 className="header-slogan">Crafting Precision for Solar Success</h3>
     </div>
   );
