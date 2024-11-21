aee0f29dbc9fb2dcc573c41e5b775b5398f49ac8
username <Jzazzi56@gmail.com>
2024-11-21 11:29:03 -0800

    simple trademark footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <h5 className="footer">&copy; 2024 Terrific Every Design</h5>
+    </div>
+  );
+};
+
+export default Footer;
