d1f1b19897688ab305a468efd8d902107d8df462
username <Jzazzi56@gmail.com>
2024-11-21 11:38:30 -0800

    added footer to home page

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,6 +2,7 @@ import Header from "../../components/header/header";
 import Contact from "../../components/contact/contact";
 import Navbar from "../../components/navbar/navbar";
 import RequestGraphWithErrorBoundary from "../../components/request-graph/requestGraph";
+import Footer from "../../components/footer/Footer";
 import "./home.css";
 
 const home = () => {
@@ -11,6 +12,7 @@ const home = () => {
       <Header />
       <RequestGraphWithErrorBoundary />
       <Contact />
+      <Footer />
     </div>
   );
 };
