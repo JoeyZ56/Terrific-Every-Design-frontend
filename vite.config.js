8ad9cda3fb1a696be83ef19ee75340d68ea898d4
username <Jzazzi56@gmail.com>
2024-11-21 09:43:40 -0800

    changed and secured port

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.VITE_PORT || 5173;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: PORT,
+  },
+});
